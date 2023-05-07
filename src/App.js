17d1ccf88d4f563b1cba425bd85f98fb79cf089d
patramonalisa <patramonalisa20@gmail.com>
2023-05-07 17:43:22 +0530

    added code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Quizs from './components/Quizs'
+import "./App.css"
+function App() {
+  return (
+    <>
+    <Quizs />
+    
+    </>
+    
+  );
+}
+
+export default App;
